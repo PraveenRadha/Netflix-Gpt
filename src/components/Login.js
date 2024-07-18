ba83bc33b1d4792d1c800ad570c806370a8d4745
Praveen345687687 <211501066@rajalakshmi.edu.in>
2024-07-18 21:24:07 +0530

    create sign in and signup validation for login and firebase setup was completed sucessfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,27 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 import auth from "../utils/firebase"
 
 const Login = () => {
     const [isSignInForm,setIsSignInForm]=useState(true);
     const [errorMessage,setErrorMessage]=useState(null);
     
-    const name=useRef(null);
+    // const name=useRef(null);
     const email = useRef(null);
     const password = useRef(null);
     
     const handleButtonClick = () => {
-        
+
         //validate the form data
-        const message = checkValidData(name.current.value,email.current.value,password.current.value);
+        const message = checkValidData(email.current.value,password.current.value);
         setErrorMessage(message);
             //create a new useer in firebase.(sign in .sign up)
         if(message)return;
         //sign in ans sign up
         if(!isSignInForm){
-            
-    createUserWithEmailAndPassword(auth,name.current.value,email.current.value,password.current.value)
+    createUserWithEmailAndPassword(auth,email.current.value,password.current.value)
     .then((userCredential) => {
     // Sign uplogic 
     const user = userCredential.user;
@@ -31,14 +30,23 @@ const Login = () => {
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
-    setErrorMessage(errorCode + "-" + errorCode);
+    setErrorMessage(errorCode + "-" + errorMessage);
   });
        
-    }else{
-        //sign in logic
     }
-        
-            
+    else{
+        //sign in logic
+        signInWithEmailAndPassword(auth, email.current.value,password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+    } 
         
     };
 
@@ -46,11 +54,12 @@ const Login = () => {
         setIsSignInForm(!isSignInForm);
     }
   return (
-    <div>
+    <div >
         <Header />
         <div className="absolute">
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/8728e059-7686-4d2d-a67a-84872bd71025/e90516bd-6925-4341-a6cf-0b9f3d0c140a/IN-en-20240708-POP_SIGNUP_TWO_WEEKS-perspective_WEB_34324b52-d094-482b-8c2a-708dc64c9065_small.jpg" 
             alt="logo"
+            // className="absolute object-cover w-full h-full"
             />
         </div>
         <form onSubmit={(e) => e.preventDefault()}
@@ -59,7 +68,8 @@ const Login = () => {
                 {isSignInForm ? "Sign In" : "Sign up"}
             </h1>
             {!isSignInForm && (
-            <input ref={name}
+             <input 
+            //  ref={name}
              type="text" placeholder="Full name" className="w-full p-2 my-4 bg-gray-700 rounded-lg" />
 
             ) }
